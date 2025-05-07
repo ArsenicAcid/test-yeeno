b9ba9db086a84992cdb6b1be21ee3b238ad633cf
Arsenic <russo03pac@hotmail.com>
2025-05-07 19:08:57 +0200

    [FE] Removed force on attribute and added it to integrity check

--- src/app/components/ui/card.tsx
+++ src/app/components/ui/card.tsx
@@ -9,12 +9,12 @@ type cardProps = {
 const Card = (props: cardProps) => {
     const attendee = props.cardAttendee
 
-    if (!attendee.profilePictureUrl) return
+    if (!attendee.profilePictureUrl || !attendee.fursonaName) return
 
 
     return (
         <div className='flex flex-col w-40 h-60'>
-            <Image src={attendee.profilePictureUrl} alt={attendee.fursonaName!} width={100} height={100} className='w-full h-auto' loading='lazy'/>
+            <Image src={attendee.profilePictureUrl} alt={attendee.fursonaName} width={100} height={100} className='w-full h-auto' loading='lazy'/>
             <h1>{attendee.fursonaName}</h1>
         </div>
   )
