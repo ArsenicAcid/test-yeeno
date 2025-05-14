597b10e7441b07f8e36c2269c5ee74ec78854997
Arsenic <russo03pac@hotmail.com>
2025-05-14 17:19:07 +0200

    [FE] Implemented filters

--- src/app/components/attendees/attendees.tsx
+++ src/app/components/attendees/attendees.tsx
@@ -1,29 +1,75 @@
+'use client'
 import React from 'react'
 import Card from '../ui/card'
 import { attendee } from '../interfaces/attendee-interface'
 
-const AttendeesComponent = async () => {
-    const {error, success} = await getAttendees()
-    if (error) return error
-    const attendees : attendee[] = await success?.json()
+const AttendeesComponent = (props: any) => {
+    const [search, setSearch] = React.useState('')
+    const [kindFilter, setKindFilter] = React.useState('all')
+    const [isFursuiterFilter, setIsFursuiterFilter] = React.useState('all')
+    const [ticketKindFilter, setTicketKindFilter] = React.useState('all')
+    const cardAttendees =  props.attendees
+    
+    const handleKindFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setKindFilter(event.target.value)
+    }
+
+    const handleIsFursuiterFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setIsFursuiterFilter(event.target.value)
+    }
+
+    const handleTicketKindFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setTicketKindFilter(event.target.value)
+    }
+
+    const filteredAttendees = cardAttendees.filter((attendee: attendee) => {
+        const searchMatch = attendee.fursonaName?.toLowerCase().includes(search.toLowerCase())
+        const kindMatch = kindFilter === 'all' || attendee.kind === kindFilter
+        const isFursuiterMatch = isFursuiterFilter === 'all' || attendee.isFursuiter?.toString() === isFursuiterFilter
+        const ticketKindMatch = ticketKindFilter === 'all' || attendee.ticketKind === ticketKindFilter
+        return searchMatch && kindMatch && isFursuiterMatch && ticketKindMatch
+    })
+
     return (
-        <div className="flex items-center flex-col md:grid md:grid-cols-3 lg:grid-cols-4 md:justify-items-center gap-4 w-full md:w-auto">
-            {attendees.map((attendee : attendee, index : number) => (
-            <Card key={index} cardAttendee={attendee} />
-          ))}
+        <div className="flex flex-col items-center w-full">
+            <div className="w-full flex gap-2 h-12 mt-4">
+                <form>
+                    <input  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3" 
+                            type="search" 
+                            id="searchbar" 
+                            name="searchbar" 
+                            placeholder="Search attendees by Name" 
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                </form>
+                    <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-3" value={kindFilter} onChange={handleKindFilterChange}>
+                        <option value="all">All Kinds</option>
+                        <option value="staff">Staff</option>
+                        <option value="attendee">Attendee</option>
+                        <option value="dealer">Dealer</option>
+                        <option value="security">Security</option>
+                        <option value="bar">Bar</option>
+                        <option value="av/it">AV/IT</option>
+                        <option value="wildcard">Wildcard</option>
+                    </select>
+                    <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-3" value={isFursuiterFilter} onChange={handleIsFursuiterFilterChange}>
+                        <option value="all">All</option>
+                        <option value="true">Fursuiters</option>
+                        <option value="false">Non-fursuiters</option>
+                    </select>
+                    <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-3" value={ticketKindFilter} onChange={handleTicketKindFilterChange}>
+                        <option value="all">All Ticket Kinds</option>
+                        <option value="base">Base</option>
+                        <option value="early">Early</option>
+                    </select>
+            </div>
+            <div className="flex items-center flex-col md:grid md:grid-cols-3 lg:grid-cols-4 md:justify-items-center gap-4 w-full">
+                {filteredAttendees.map((attendee: attendee, index: number) => (
+                    <Card key={index} cardAttendee={attendee} />
+                ))}
+            </div>
         </div>
     )
 }
 
-const getAttendees = async () => {
-    try {
-        const reqURL = "http://lesgoski.ddns.net:10621/api/pun25/attendees/flat-list"
-        const data = await fetch(reqURL)
-        return { success: data}
-    } catch (error) {
-        return { error: "Failed to fetch attendees"}
-    }
-}
-
 export default AttendeesComponent
-  
