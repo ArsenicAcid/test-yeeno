59761ec536e5c31b64d2f0d193fd79c6b897c8a0
Arsenic <russo03pac@hotmail.com>
2025-05-14 17:18:55 +0200

    [BE] Moved query to attendees page and turned attendees + card components to client

--- src/app/attendees/page.tsx
+++ src/app/attendees/page.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import AttendeesComponent from '../components/attendees/attendees'
+import { attendee } from '../components/interfaces/attendee-interface'
 
 
+const AttendeesPage = async () => {
 
-const AttendeesPage = () => {
+  const {error, success} = await getAttendees()
+    if (error) return error
+    const attendees : attendee[] = await success?.json()
+
     return (
       <div className={"flex flex-col w-full items-center"}>
-        <AttendeesComponent />
+        <AttendeesComponent attendees={attendees} />
       </div>
     )
+}
+
+const getAttendees = async () => {
+  try {
+      const reqURL = "http://lesgoski.ddns.net:10621/api/pun25/attendees/flat-list"
+      const data = await fetch(reqURL)
+      return { success: data}
+  } catch (error) {
+      return { error: "Failed to fetch attendees"}
+  }
 }
   
   export default AttendeesPage
