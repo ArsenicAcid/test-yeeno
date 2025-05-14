9ae347ba816daf77039d949ac1b1b93fbdf816ac
Arsenic <russo03pac@hotmail.com>
2025-05-14 15:04:53 +0200

    [BE/FE] Detached Ajax from main component render and implemented error handling

--- src/app/components/attendees/attendees.tsx
+++ src/app/components/attendees/attendees.tsx
@@ -3,9 +3,9 @@ import Card from '../ui/card'
 import { attendee } from '../interfaces/attendee-interface'
 
 const AttendeesComponent = async () => {
-    const reqURL = "http://lesgoski.ddns.net:10621/api/pun25/attendees/flat-list"
-    const res = await fetch(reqURL)
-    const attendees : attendee[] = await res.json()
+    const {error, success} = await getAttendees()
+    if (error) return error
+    const attendees : attendee[] = await success?.json()
     return (
         <div className="flex items-center flex-col md:grid md:grid-cols-3 lg:grid-cols-4 md:justify-items-center gap-4 w-full md:w-auto">
             {attendees.map((attendee : attendee, index : number) => (
@@ -13,6 +13,16 @@ const AttendeesComponent = async () => {
           ))}
         </div>
     )
+}
+
+const getAttendees = async () => {
+    try {
+        const reqURL = "http://lesgoski.ddns.net:10621/api/pun25/attendees/flat-list"
+        const data = await fetch(reqURL)
+        return { success: data}
+    } catch (error) {
+        return { error: "Failed to fetch attendees"}
+    }
 }
 
 export default AttendeesComponent
