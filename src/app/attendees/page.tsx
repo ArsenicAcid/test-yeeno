11d4c574e7b63a3a278c95407ffc9760813a3e55
Arsenic <russo03pac@hotmail.com>
2025-05-21 20:10:56 +0200

    [BE] Added check for attendees response on page after ajax

--- src/app/attendees/page.tsx
+++ src/app/attendees/page.tsx
@@ -8,7 +8,8 @@ const AttendeesPage = async () => {
   const {error, success} = await getAttendees()
     if (error) return error
     const attendees : attendee[] = await success?.json()
-
+    if (!attendees) return
+    
     return (
       <div className={"flex flex-col w-full items-center"}>
         <AttendeesComponent attendees={attendees} />
@@ -20,7 +21,7 @@ const getAttendees = async () => {
   try {
       const reqURL = "http://lesgoski.ddns.net:10621/api/pun25/attendees/flat-list"
       const data = await fetch(reqURL)
-      return { success: data}
+            return { success: data}
   } catch (error) {
       return { error: "Failed to fetch attendees. Error: " + error}
   }
